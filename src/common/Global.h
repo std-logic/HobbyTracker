01f0e7a67edea6dae898366ddb785cc002d96a86
std-logic <jackstdlogic@gmail.com>
2026-06-29 15:52:37 +0400

    All global constants have external linkage now.

--- src/common/Global.h
+++ src/common/Global.h
@@ -9,15 +9,15 @@ namespace Global
 {
 	inline const char settings_file_name[] = "settings.ini";
 
-	const uint32_t undefined_value = 0;
+	inline const uint32_t undefined_value = 0;
 
 	namespace Colors
 	{
-		const QColor tree_level_1 = QColor(240, 240, 240);
-		const QColor tree_level_2 = QColor(230, 230, 230);
-		const QColor tree_level_3 = QColor(220, 220, 220);
+		inline const QColor tree_level_1 = QColor(240, 240, 240);
+		inline const QColor tree_level_2 = QColor(230, 230, 230);
+		inline const QColor tree_level_3 = QColor(220, 220, 220);
 
-		const QColor rating[10] =
+		inline const QColor rating[10] =
 		{
 			QColor(255, 100, 100), // 1
 			QColor(250, 150, 150), // 2
@@ -46,14 +46,14 @@ namespace Global
 
 	namespace Sizes
 	{
-		const int window_min_width = 960;
-		const int window_min_height = 540;
+		inline const int window_min_width = 960;
+		inline const int window_min_height = 540;
 
-		const int font_small = 9;
-		const int font_big = 11;
+		inline const int font_small = 9;
+		inline const int font_big = 11;
 
-		const int default_spacing = 5;
-		const QMargins default_margin = {5, 5, 5, 5};
+		inline const int default_spacing = 5;
+		inline const QMargins default_margin = {5, 5, 5, 5};
 	} // namespace Sizes
 
 } // namespace Global
